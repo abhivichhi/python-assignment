feb533fa8e9212b291c7611d172ae8caa7662dfb
Abhishek <abhi.vichhi@gmail.com>
2025-06-03 17:12:09 +0530

    Uploading of  Assignment1

---
+++ ASSIGNMENT-1/task1.py
@@ -0,0 +1,12 @@
+num1 = input("Enter the first number: ")
+num2 = input("Enter the second number: ")
+
+addValue = int(num1) + int(num2)
+subValue = int(num1) - int(num2)
+mulValue = int(num1) * int(num2)
+divValue = int(num1) / int(num2)
+
+print("Addition: ", addValue)
+print("Subtraction: ", subValue)
+print("Multiplication: ", mulValue)
+print("Division: ", divValue)
---
+++ ASSIGNMENT-1/task2.py
@@ -0,0 +1,6 @@
+firstname = input("Enter your first name: ")
+lastname = input("Enter your last name: ")
+
+completename = str(firstname) + " " + str(lastname)
+
+print("Hello, " + completename + "! Welcome to the Python program.")
